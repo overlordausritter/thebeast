213670b9f406f9a3d970146830bb76ef08aaa8ef
overlordausritter <ar@generalinnovation.com>
2025-10-27 13:41:21 -0400

    Update llamaquery_api.py

--- llamaquery_api.py
+++ llamaquery_api.py
@@ -116,6 +116,3 @@ async def llamaquery(request: Request):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
